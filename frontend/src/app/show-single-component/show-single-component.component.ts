00b9b4f05678784bfce29f9bfe44c5fb03bddd14
schrabsi126 <schrabsi126@gmail.com>
2019-12-18 15:29:18 +0100

    add components

---
+++ frontend/src/app/show-single-component/show-single-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-single-component',
+  templateUrl: './show-single-component.component.html',
+  styleUrls: ['./show-single-component.component.css']
+})
+export class ShowSingleComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/show-storage/show-storage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-storage',
+  templateUrl: './show-storage.component.html',
+  styleUrls: ['./show-storage.component.css']
+})
+export class ShowStorageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
